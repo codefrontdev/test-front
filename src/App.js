3f479cff2c735bc0c9e8f660351e1fbb52f7ac8e
codefrontdev <118992125+codefrontdev@users.noreply.github.com>
2023-08-17 07:59:41 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ProtectedRoute from './components/Routes/ProtectedRoute';
+import PublicRoute from './components/Routes/PublicRoute';
+
+
+function App() {
+  return (
+    <>
+    <ToastContainer position='top-center' />
+      <Routes>
+        <Route path='/' element={
+          <ProtectedRoute>
+            <HomePage/>
+          </ProtectedRoute>
+        }/>
+        <Route path='/login' element={
+          <PublicRoute>
+            <Login/>
+          </PublicRoute>
+        } />
+        <Route path='/register' element={
+          <PublicRoute>
+            <Register/>
+          </PublicRoute>
+        } />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
